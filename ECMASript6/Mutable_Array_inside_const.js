4e79726b2ad73c06a1bf5683d07d30916f99ae6c
Sarthak Gautam <105770816+imSarthakGautam@users.noreply.github.com>
2023-07-06 18:44:42 +0545

    ES6

--- ECMASript6/Mutable_Array_inside_const.js
+++ ECMASript6/Mutable_Array_inside_const.js
@@ -1,6 +1,6 @@
 const s = [5, 7, 2];
 function editInPlace() {
-  // Only change code below this line
+  
 
   // Using s = [2, 5, 7] would be invalid
 
@@ -8,6 +8,6 @@ function editInPlace() {
   s[0] = 2;
   s[1] = 5;
   s[2] = 7;
-  // Only change code above this line
+  
 }
 editInPlace();
